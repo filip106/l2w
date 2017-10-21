d675a8f2cc23ae2f15474d36f74a9540061db041
filip <djordjevic.filip994@gmail.com>
2017-10-21 11:29:54 +0200

    Created header for homepage.

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="blog-page")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function blogPageAction(Request $request)
+    {
+        return $this->render('@App/frontend/blog/blog.html.twig', array());
+    }
+
+    /**
+     * @Route("/blog/{slug}", name="blog-single")
+     * @param Request $request
+     * @param string $slug
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function blogSingleAction(Request $request, $slug)
+    {
+        return $this->render('@App/frontend/blog/blog-single.html.twig', array(
+            'slug' => $slug
+        ));
+    }
+}
--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -6,14 +6,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+/**
+ * Class FrontEndController
+ * @package AppBundle\Controller
+ */
+class IndexController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('@App/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);

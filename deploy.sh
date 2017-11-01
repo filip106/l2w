99ffa65eba304d1cdd16b3885e5c92728cc7acdf
Filip <djordjevic.filip994@gmail.com>
2017-11-01 20:05:20 +0100

    Change assets.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "deploy started";
-php bin/console cache:clear
+php bin/console cache:clear --no-warmup
+php bin/console cache:warmup
 php bin/console assets:install
 php bin/console assetic:dump
 echo "deploy finished";
